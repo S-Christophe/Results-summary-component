480fe5622ea6e9fedc9d48a78e64be47ac5bc469
shaochris300 <christophe300@hotmail.com>
2023-09-27 15:04:41 +0200

    update next config js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,20 @@
 /** @type {import('next').NextConfig} */
 
-module.exports = (phase, {defaultConfig}) => {
-    if ('sassOptions' in defaultConfig) {
-        defaultConfig['sassOptions'] = {
-            includePaths: ['./src/app/styles'],
-            prependData: `@import "~@styles/fonts.scss"; @import "~@styles/variables.scss";`,
-        }
-    }
-    return defaultConfig;
+// module.exports = (phase, {defaultConfig}) => {
+//     if ('sassOptions' in defaultConfig) {
+//         defaultConfig['sassOptions'] = {
+//             includePaths: ['./src/app/styles'],
+//             prependData: `@import "~@styles/fonts.scss"; @import "~@styles/variables.scss";`,
+//         }
+//     }
+//     return defaultConfig;
+// }
+
+module.exports = {
+    output: 'export',
+    /* Add Your Scss File Folder Path Here */
+    sassOptions: {
+        includePaths: ['./src/app/styles'],
+        prependData: `@import "~@styles/fonts.scss"; @import "~@styles/variables.scss";`,
+    },
 }
